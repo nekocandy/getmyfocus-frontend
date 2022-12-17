fbddf6716720748e9a96fd529471fcb2e4432af8
Piyush <bhangalepiyush@gmail.com>
2022-12-17 23:31:33 +0530

    feat: slate -> stone

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import routes from "~react-pages";
 
 export default function App() {
   return (
-    <div className="h-screen bg-slate-900">
+    <div className="h-screen bg-stone-900">
       <div className="h-full max-w-7xl mx-auto">
         <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
       </div>
