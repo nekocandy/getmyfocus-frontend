ff5b1bdfa65f9fd4c611029d52049aef60d0c584
Piyush <bhangalepiyush@gmail.com>
2022-12-18 01:48:13 +0530

    feat: more responsiveness and center on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,14 +15,16 @@ export default function HomePage() {
           <span className="text-lg">Hi {user.name},</span>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+        <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {new Array(10).fill(0).map((_, index) => (
-            <SessionCard
-              key={index}
-              index={index + 1}
-              id={index.toString()}
-              sessionName="Hello"
-            />
+            <div>
+              <SessionCard
+                key={index}
+                index={index + 1}
+                id={index.toString()}
+                sessionName="Hello"
+              />
+            </div>
           ))}
         </div>
       </div>
